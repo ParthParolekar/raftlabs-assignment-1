2e894ba25c540a51585022dda70032d0cd7302c6
ParthParolekar <parolekarparth@gmail.com>
2022-09-08 13:31:21 +0530

    add: routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import { AddUser, Home } from "./pages";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/adduser" element={<AddUser />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,4 @@
+import Home from "./Home/Home";
+import AddUser from "./AddUser/AddUser";
+
+export { Home, AddUser };
