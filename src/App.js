3eb7077f0de83e36d8d566391f17ca998cf2ae73
ParthParolekar <parolekarparth@gmail.com>
2022-09-08 13:57:40 +0530

    add: route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import AddUser from "./pages/AddUser/AddUser";
+import Home from "./pages/Home/Home";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/addUser" element={<AddUser />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
